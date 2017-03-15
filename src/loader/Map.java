bb40fd1e7b3b7cc1c7b0d6f13fd842840b9ebf2a
Luke Biddell <lxb550@cs.bham.ac.uk>
2017-03-15 23:08:33 +0000

    Added Map and Tile classes

---
+++ src/loader/Map.java
@@ -0,0 +1,12 @@
+package loader;
+
+public class Map {
+	Tile[][] tiles;
+	
+	
+	public Map(int length, int width) {
+		tiles = new Tile[length][width];
+		
+	}
+
+}
---
+++ src/loader/Tile.java
@@ -0,0 +1,9 @@
+package loader;
+
+public class Tile {
+
+	public Tile() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
