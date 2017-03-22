a3dabe00265cef0cadd2bd2c295ec0366010d4bb
oms567 <oms567@cs.bham.ac.uk>
2017-03-22 04:33:20 +0000

    monsters now follow the player that's closest to them

--- src/game/Monster2.java
+++ src/game/Monster2.java
@@ -6,15 +6,9 @@ public class Monster2 extends Monster{
 	public Monster2(Game game, Point2D.Double pos){
 		super(game, 10, 0.3, pos, 100, 100, new Animation(SpritesheetEnum.MONSTER2, 0, 0, 0.2, Animation.AnimationMode.LOOP));
 		
-		try{
-			Player towards = game.players.peek();
-			if(towards == null) throw new NoSuchElementException();
-				//addBehaviour(new JumpAround(1.8, 2.4));
-				addBehaviour(new LinearHome(0.18, towards.getPos()));
-				  addBehaviour(new RotateBehaviour(0.7, 0.3, pos));
-		}
-		catch(NoSuchElementException e){}
+		//addBehaviour(new JumpAround(1.8, 2.4));
+		addBehaviour(new GreedyFollowBehaviour(0.18));
 		
-		 addBehaviour(new RotateBehaviour(0.7, 0.6, pos));
+		addBehaviour(new RotateBehaviour(0.7, 0.6, pos));
 	}
 }
