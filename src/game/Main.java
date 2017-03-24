224c4ce42170c4e3702315a56dd5fde151568ffa
Luke Biddell <lxb550@cs.bham.ac.uk>
2017-03-24 07:36:44 +0000

    Fixed levels

--- src/game/Main.java
+++ src/game/Main.java
@@ -21,7 +21,7 @@ public class Main extends Thread {
 //		bgm.update();
 		
 	   long lastLoopTime = System.nanoTime();
-	   final int TARGET_FPS = 60;
+	   final int TARGET_FPS = 30;
 	   final long OPTIMAL_TIME = 1000000000 / TARGET_FPS;
 	   
 	   long lastFpsTime = 0;
